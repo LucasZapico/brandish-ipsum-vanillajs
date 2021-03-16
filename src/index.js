1064def09d1054338f02eb56d787eb23fca25328
Lucas Zapico <zapicolucas@gmail.com>
2021-03-15 20:29:47 -0600

    add test it out functionality and error states

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import Icons from 'uikit/dist/js/uikit-icons.js';
 import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+
 import './styles.scss';
 
 // loads the Icon plugin
@@ -9,10 +10,40 @@ console.log('api url', process.env.API_BASE_URL);
 
 const urlInput = document.getElementById('url');
 const ipsum = document.getElementById('ipsum');
+const root = document.getElementById('root');
 const parSize = document.querySelectorAll('[type="radio"]');
 const form = document.querySelector('form');
+
 const toClipboard = document.getElementById('to-clipboard');
 console.log('submit', form);
+
+async function handleQuery(query) {
+  const response = await fetch(query, {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Methods': 'POST,GET,PATCH,DELETE',
+      'Content-Type': 'application/json, charset=utf-8',
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+test.addEventListener('click', (e) => {
+  test.innerText = 'Testing';
+  makeSkeleton();
+  const query = `${process.env.API_BASE_URL}/make-ipsum/?url=https://roaveyewear.com/&number-para=2&para-size=150`;
+  // const url = `http://127.0.0.1:5000/get-ipsum/`;
+  handleQuery(query)
+    .then(({ data }) => {
+      test.innerText = 'Test it out';
+      makeIpsumParagraphs(data.ipsum);
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+});
 
 toClipboard.addEventListener('click', (e) => {
   const selection = window.getSelection();
@@ -37,16 +68,9 @@ toClipboard.addEventListener('click', (e) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('url', e.target.url);
   if (e.target.url.value !== '') {
     makeSkeleton();
-    console.log('subm', e);
     e.target.submit.innerText = 'Making Ipsum';
-    setTimeout(() => {
-      e.target.submit.innerText = 'Make Ipsum';
-      e.target.submit.classList.remove('success');
-    }, 1200);
-
     // get paragraph size
     let paraSize = 10;
     parSize.forEach((i) => {
@@ -54,35 +78,45 @@ form.addEventListener('submit', (e) => {
         paraSize = i.id;
       }
     });
-    const url = `${process.env.API_BASE_URL}/make-ipsum/?url=${urlInput.value}&number-para=${e.target.paragraphs.value}&para-size=${paraSize}`;
+    const query = `${process.env.API_BASE_URL}/make-ipsum/?url=${urlInput.value}&number-para=${e.target.paragraphs.value}&para-size=${paraSize}`;
     // const url = `http://127.0.0.1:5000/get-ipsum/`;
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Methods': 'POST,GET,PATCH,DELETE',
-        'Content-Type': 'application/json, charset=utf-8',
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
+    handleQuery(query)
       .then((response) => {
-        return response.json();
+        console.log('response', response);
+        if (response.data) {
+          const { data } = response;
+          e.target.submit.innerText = 'Make Ipsum';
+          makeIpsumParagraphs(data.ipsum);
+        } else if (response.error) {
+          console.log('error in response');
+          handleResponseError(response.error);
+        }
       })
-      .then(({ data }) => {
-        makeIpsumParagraphs(data.ipsum);
-      })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        e.target.submit.innerText = 'Make Ipsum';
+        console.log('error running');
+        console.log('error', error);
+        return error;
       });
   } else {
-    console.log('error');
-    const missingUrl = document.createElement('div');
-    missingUrl.setAttribute('class', 'uk-alert-warning');
-
-    missingUrl.innerHTML =
-      '<a class="uk-alert-close" uk-close></a><p>Hey! We need a url to make ipsum!</p>';
-    e.target.url.parentElement.appendChild(missingUrl);
+    handleEmptyUrl();
   }
 });
+
+function handleEmptyUrl() {
+  const missingUrl = document.createElement('div');
+  missingUrl.innerHTML = `<div class="uk-alert-warning" uk-alert>
+  <a class="uk-alert-close" uk-close></a> <p>Hey! We need a url to make ipsum!</p></div>`;
+  root.insertBefore(missingUrl, form);
+}
+
+function handleResponseError(error) {
+  const errorMessage = document.createElement('div');
+
+  errorMessage.innerHTML = `<div class="uk-alert-danger" uk-alert><a class="uk-alert-close" uk-close></a><p>${error.message}</p></div>`;
+  root.insertBefore(errorMessage, form);
+  ipsum.innerHTML = '<br />';
+}
 
 function makeIpsumParagraphs(ipsumArr) {
   if (ipsum.childElementCount > 1) {
